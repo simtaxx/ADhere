6e6b0764ae2a8160842aafa85bbd6ed980340799
simtaxx <souici.haris@gmail.com>
2020-02-14 00:12:26 +0100

    element page map

---
+++ src/components/ui/stationsCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const StationsCard = () => {
+  
+  return(
+    <div className="stations-card">
+      <div className="stations-card__title-container">
+        <div className="stations-card__icon"></div>
+        <h4 className="stations-card__title">Tennis | du 28/07/24 au 4/08/24 | Stade Roland-Garros</h4>
+      </div>
+      <div className="stations-card__stats-container">
+        <div className="stations-card__stat">
+          <p className="stations-card__stat-text">Audience ciblé</p>
+          <p className="stations-card__stat-numbers">18 300</p>
+        </div>
+        <div className="stations-card__stat">
+          <p className="stations-card__stat-text">AUDIENCE TOTALE</p>
+          <p className="stations-card__stat-numbers">70 000</p>
+        </div>
+        <div className="stations-card__stat">
+          <p className="stations-card__stat-text">Stations aux alentours</p>
+          <p className="stations-card__stat-numbers">3 stations</p>
+        </div>
+        <div className="stations-card__stat">
+          <p className="stations-card__stat-text">Stations Séléctionnées</p>
+          <p className="stations-card__stat-numbers">0 station</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default StationsCard
--- src/views/stations.jsx
+++ src/views/stations.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Header from '../components/layouts/header'
 import Breadcrumb from '../components/modules/breadcrumb'
+import StationsCard from '../components/ui/stationsCard'
 
 const Audience = () => {
   
@@ -13,6 +14,7 @@ const Audience = () => {
           <h2>Localisation des épreuves et séléction des gares</h2>
           <h3>Sélectionnez les événement qui vous conviennent</h3>
           <p className="description">Vous pourrez après choisir les gares a proximité où vous pourrez afficher votre  publicité </p>
+          <StationsCard></StationsCard>
         </div>
         <div className="semi-container">
           <Breadcrumb pathRef="stations" />
