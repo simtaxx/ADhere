c1387fd101255f55ca6dc91be4678f98ef1d1cba
Donaël Walt <donael.walter@hotmail.com>
2020-02-22 13:35:43 +0100

    vvvv

--- src/views/dataviz.jsx
+++ src/views/dataviz.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Doughnut, Line} from 'react-chartjs-2';
+import Map from '../components/modules/map';
 
 import Header from '../components/layouts/header'
 
@@ -77,15 +78,51 @@ const DataViz = ()=>{
     };
 
 
+    
+  var dataEventLocation = [
+    {
+      name: "Gare de Lyon",
+      location: [48.8443038, 2.3743773],
+      stations: 0
+    },
+    {
+      name: "Chatelet les Halles",
+      location: [48.8620543, 2.3449645],
+      stations: 0
+    },
+    {
+      name: "Nation ",
+      location: [48.8483634, 2.393789],
+      stations: [
+        {
+          name: "rue des boulet",
+          location: [48.8522705, 2.3872059]
+        },
+        {
+          name: "picpus",
+          location: [48.8449438, 2.3990587]
+        }
+      ]
+    }
+  ]; 
+
+  var iconUrlChallenge = 'http://localhost:3000/epreuve.png';
+  var iconUrlStation = 'http://localhost:3000/station.png';
+
     return(
         <div className="dataviz">
             <Header title="Visualisation du processus"></Header>
-            <div className="small-gap">
-             <div style={{display: 'grid', gridTemplateColumns:"1fr 1fr", width: '300px', gridAutoRows:'300px'}}>
+            <div style={{width: '100vw', height:'90vh', display:'flex', flexDirection:'row'}}>
+            <div style={{width:'50vw', textAlign:'center', display:'flex', alignContent:'center', alignItems:'center', justifyContent:'center', justifyItems:'center'}}>
+                <div style={{display: 'grid', gridTemplateColumns:"1fr 1fr", width: '300px', gridAutoRows:'300px', marginRight:'300px'}}>
                  <Doughnut data={data} />
                  <Doughnut data={data1} />
                  <Doughnut data={data2} />
                  <Doughnut data={data3} />
+                 </div>
+            </div>
+             <div style={{width:'50vw'}}>
+                <Map height="90vh"  eventLocationData={{dataEventLocation}} iconUrl={{iconUrlChallenge,iconUrlStation}}/>
              </div>
             </div>
         </div>
