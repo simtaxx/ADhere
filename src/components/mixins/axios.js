9f83d95cc4cd33d78a5c4cb3b4cb87cc17eda04d
simtaxx <souici.haris@gmail.com>
2020-02-24 00:39:42 +0100

    up

--- src/components/mixins/axios.js
+++ src/components/mixins/axios.js
@@ -1,16 +1,13 @@
 import Axios from 'axios'
 
 const AxiosGet = async (url, params) => {
-  const response = await Axios.get(url, params,   
-      {
-        headers : { accept : 'application/json' }
-      }
-    )
+  const response = await Axios.get( url, params )
+  console.log(response)
   return response
 }
-
+/* 
 const AxiosPost = async (url, params) => {
   await Axios.post( url, params )
 }
-
-export { AxiosGet, AxiosPost }
+ */
+export default AxiosGet
